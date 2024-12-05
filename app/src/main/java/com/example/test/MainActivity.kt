303c0e600cac993e3f99866caac43c895b5fece0
{yasaiseikatu} <{kd1314617@st.kobedenshi.ac.jp}>
2024-12-05 12:03:59 +0900

    test

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+//    test
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
