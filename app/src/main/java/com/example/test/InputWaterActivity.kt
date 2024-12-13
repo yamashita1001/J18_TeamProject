46c655048e53b10f36eec1a894c472727b03da02
cafe-au-lait02 <kd1346679@st.kobedenshi.ac.jp>
2024-12-13 10:13:24 +0900

    水分入力更新

--- app/src/main/java/com/example/test/InputWaterActivity.kt
+++ app/src/main/java/com/example/test/InputWaterActivity.kt
@@ -6,14 +6,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class InputWaterActivity : AppCompatActivity() {
 
-    // 水分摂取量追加時の時間格納
-    private var actionTimestamp: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +21,11 @@ class InputWaterActivity : AppCompatActivity() {
         // UIに表示
         val inputCompletedButton = findViewById<Button>(R.id.inputCompletedButton)
         val inputNumberText = findViewById<EditText>(R.id.inputNumberText)
+        val button120 = findViewById<ImageButton>(R.id.button120)
+        val button500 = findViewById<ImageButton>(R.id.button500)
+        val button1000 = findViewById<ImageButton>(R.id.button1000)
 
-
-
-//        welcomeTextView.text = "ようこそ、$username さん！"
+        // welcomeTextView.text = "ようこそ、$username さん！"
 
         // Homeへ遷移(入力した値を持って)
         inputCompletedButton.setOnClickListener {
@@ -42,24 +42,26 @@ class InputWaterActivity : AppCompatActivity() {
                 Toast.makeText(this, "1以上6000以下の値を入力してください", Toast.LENGTH_SHORT).show()
             }
             else {
-                // ボタンが押された時の時刻を記録
-                actionTimestamp = System.currentTimeMillis()
                 // 値が範囲内ならホーム画面へ遷移
                 val intent = Intent(this, HomeActivity::class.java)
                 // intent変数をつなげる(第一引数はキー，第二引数は渡したい変数)
                 intent.putExtra("TEXT_KEY", waterValue)
-                intent.putExtra("TIMESTAMP_KEY", actionTimestamp)
                 startActivity(intent)
             }
         }
 
+        button120.setOnClickListener {
+            // 数値を足して直接ホームに戻るか下の手入力テキストエリアに一旦表示するか
+            // 一旦手入力のテキストエリアに数値を入れる
+            inputNumberText.setText("120")
+        }
 
+        button500.setOnClickListener {
+            inputNumberText.setText("500")
+        }
 
-
-
-
-
-
-
+        button1000.setOnClickListener {
+            inputNumberText.setText("1000")
+        }
     }
 }
